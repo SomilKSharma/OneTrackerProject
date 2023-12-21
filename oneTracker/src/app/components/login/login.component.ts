d62b6314c8730126fa1cb53e0c160788fa8bbe79
Somil Sharma <somil81@icloud.com>
2023-12-21 15:09:12 +0530

    Animation

--- oneTracker/src/app/components/login/login.component.ts
+++ oneTracker/src/app/components/login/login.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2000ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ]
 })
 export class LoginComponent {
   username: string = '';
