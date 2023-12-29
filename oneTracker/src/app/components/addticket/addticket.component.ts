85d20385d6dffb7989132456d5d21cd4c9229ab6
Somil Sharma <somil81@icloud.com>
2023-12-29 18:42:41 +0530

    Add Ticket

--- oneTracker/src/app/components/addticket/addticket.component.ts
+++ oneTracker/src/app/components/addticket/addticket.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
+import { TicketService } from '../../services/ticket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addticket',
   templateUrl: './addticket.component.html',
-  styleUrl: './addticket.component.scss'
+  styleUrls: ['./addticket.component.scss']
 })
 export class AddticketComponent {
-newTicket={
-  id: 1,
-  department: 'IT',
-  category: 'Software',
-  subCategory: 'Bug',
-  status: 'Open',
-  customer: 'John Doe',
-  issueTime: new Date('2023-01-01T10:30:00'),
-  age: 5, // in days
-  lastModifiedDate: new Date('2023-01-05T15:45:00'),
-  rootCauseAnalysis: 'Pending',
-};
+  newTicket = {
+    id: 0,
+    department: "",
+    category: "",
+    subCategory: "",
+    status: "",
+    customer: "",
+    issueTime: new Date(),
+    age: 0, // in days
+    lastModifiedDate: "",
+    rootCauseAnalysis: '',
+  };
+
+  constructor(private ticketService: TicketService, private router:Router) {}
+
+  createTicket(): void {
+    // You may want to validate form data before sending the request
+
+    this.ticketService.createTicket(this.newTicket).subscribe(
+      (createdTicket) => {
+        console.log('Ticket created successfully:', createdTicket);
+        // Optionally, you can redirect to a different page or perform other actions
+        this.router.navigate(['/dashboard/viewalltickets'])
+      },
+      (error) => {
+        console.error('Error creating ticket:', error);
+      }
+    );
+  }
 }
